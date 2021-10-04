76f4f54801c4f4760a841735bd95866cec42bdfb
Timothy Boie <admin@Timothys-MacBook-Pro.local>
2021-10-04 06:17:26 -0400

    add lock elements

--- src/A.tsx
+++ src/A.tsx
@@ -171,7 +171,16 @@ const A = (p: T) => {
           });
         }
       }}
-    />
+    >
+      {[
+        { top: 0, left: 0, right: 0 },
+        { right: 0, top: 0, bottom: 0 },
+        { bottom: 0, right: 0, left: 0 },
+        { left: 0, top: 0, bottom: 0 },
+      ].map((pos) => (
+        <div className={S.lock} style={pos} />
+      ))}
+    </div>
   );
 };
 
