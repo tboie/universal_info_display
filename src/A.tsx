f5a5e21949591bd4262f3e8bdddc3a8da2ec9974
Timothy Boie <admin@Timothys-MacBook-Pro.local>
2021-10-17 10:33:06 -0400

    enable lock click toggle

--- src/A.tsx
+++ src/A.tsx
@@ -371,6 +371,7 @@ const A = (p: T) => (
             onClick={(ev) => {
               ev.stopPropagation();
               ev.preventDefault();
+              typeof p.i !== "undefined" && TOGGLE_UNIT_LOCKS(p.i, [side]);
             }}
           ></div>
         );
