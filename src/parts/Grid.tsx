5515518ba50b517918fad8fd0781c83bce0195ec
tboie <timboie@gmail.com>
2022-04-04 21:45:23 -0400

    iheartjane photo url, decrease content page left/right border

--- src/parts/Grid.tsx
+++ src/parts/Grid.tsx
@@ -26,7 +26,8 @@ const GridItems = ({
                     "?auto=format&fit=fill&fill=solid&fillColor=%" +
                     "23fff" +
                     "&__typename=ImgixSettings&ixlib=react-9.0.2&h=100&w=100"
-                  : item["p"]
+                  : "https://uploads.iheartjane.com/cdn-cgi/image/width=200,fit=scale-down,format=auto,metadata=none/uploads/" +
+                    item["p"]
               }`}
               loading="lazy"
               alt=""
