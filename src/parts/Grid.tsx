e058884cacae5bf87d0a38cbaa6a62cb9a685f73
tboie <timboie@gmail.com>
2022-04-15 22:35:16 -0400

    experiment grid item layout

--- src/parts/Grid.tsx
+++ src/parts/Grid.tsx
@@ -18,29 +18,39 @@ const GridItems = ({
             key={item.b + item.n + idx}
             onClick={() => setSelectedItemIdx(item.id)}
           >
-            <img
-              src={`${
-                item.w === "D"
-                  ? "https://images.dutchie.com/" +
-                    item["p"] +
-                    "?auto=format&fit=fill&fill=solid&fillColor=%" +
-                    "23fff" +
-                    "&__typename=ImgixSettings&ixlib=react-9.0.2&h=200&w=200"
-                  : item["p"]
-              }`}
-              loading="lazy"
-              alt=""
-            ></img>
             <span className="title">{item.n}</span>
-            <span className="price">{"$" + item["$"]}</span>
-            <span className="type">{item["t"]}</span>
-            <span className="percent">{item["%"] ? item["%"] + "%" : ""}</span>
-            <span className="choice">
-              {item["g"] ? item["g"].join(",") : ""}
-            </span>
-            <span className="ppu">
-              {item["ppu"] ? "$" + item["ppu"] + "/g" : ""}
-            </span>
+
+            <div className="container">
+              <div className="container-text">
+                <span className="price">{"$" + item["$"]}</span>
+                <span className="choice">
+                  {item["g"] ? item["g"].join(",") : ""}
+                </span>
+                <span className="percent">
+                  {item["%"] ? Math.round(item["%"]) + "%" : ""}
+                </span>
+                <span className="type">{item["t"]}</span>
+
+                <span className="ppu">
+                  {item["ppu"] ? "$" + item["ppu"] + "/g" : ""}
+                </span>
+              </div>
+              <div className="container-img">
+                <img
+                  src={`${
+                    item.w === "D"
+                      ? "https://images.dutchie.com/" +
+                        item["p"] +
+                        "?auto=format&fit=fill&fill=solid&fillColor=%" +
+                        "23fff" +
+                        "&__typename=ImgixSettings&ixlib=react-9.0.2&h=200&w=200"
+                      : item["p"]
+                  }`}
+                  loading="lazy"
+                  alt=""
+                ></img>
+              </div>
+            </div>
           </div>
         );
       })}
