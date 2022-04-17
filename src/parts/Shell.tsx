e412593fa3eaff413a1ff42370cdc88cac2eff70
tboie <timboie@gmail.com>
2022-04-17 09:55:24 -0400

    range height style

--- src/parts/Shell.tsx
+++ src/parts/Shell.tsx
@@ -660,7 +660,6 @@ const UniversalInfoDisplay = (props: {
   }, [lat, lng, key]);
 
   const getLocation = () => {
-    /*
     setLat(0);
     setLng(0);
     setTimeout(() => {
@@ -668,14 +667,15 @@ const UniversalInfoDisplay = (props: {
       setLat(42.364506);
       setLng(-71.038887);
     }, 1000);
-    */
 
+    /*
     setLat(0);
     setLng(0);
     navigator.geolocation.getCurrentPosition((position) => {
       setLat(position.coords.latitude);
       setLng(position.coords.longitude);
     });
+    */
   };
 
   return (
